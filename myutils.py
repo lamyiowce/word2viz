e1906b7405a7e72ea60d4166caa3a892a8fca362
lamyiowce <julia@bazinski.com>
2016-07-25 14:45:40 +0200

    added graphs with t-sne, similars, myutils

---
+++ myutils.py
@@ -0,0 +1,23 @@
+def get_with_tsne(df, words, a, b):
+    axis = df.loc[b] - df.loc[a]
+    df_words = df.loc[words]
+    df_words_rest = df_words - pd.DataFrame(np.outer(df_words.dot(axis), axis), index=df_words.index, columns=df_words.columns)
+    model = TSNE(n_components=1, random_state=0, perplexity=100)
+    df_words_tsne = pd.DataFrame(model.fit_transform(df_words_rest), index=words).rename(columns={0:"tsne"})
+    df_words_tsne["name"]=words
+    df_words_tsne["axis"] = df_words.dot(axis)
+    return df_words_tsne
+
+def get_with_axes(df, words, a, b, c, d):
+    ab = df.loc[a] - df.loc[b]
+    cd = df.loc[c] - df.loc[d]
+    proj = pd.DataFrame([ab, cd], index=["ab", "cd"]).transpose()
+    df_plot = df.loc[words].dot(proj)
+    df_plot["name"] = df_plot.index
+    return df_plot
+
+def similar(dft, word, n = 10):
+    return dft.dot(word).sort_values(ascending=False).head(n)
+
+def least_similar(dft, word, n = 10):
+    return dft.dot(word).sort_values().head(n)
