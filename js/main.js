4bc114d93f5817e5e1569e99cdf4f601e046e94a
lamyiowce <julia@bazinski.com>
2016-11-05 16:40:43 +0100

    #feature adding pairs of words

--- js/main.js
+++ js/main.js
@@ -57,11 +57,13 @@ function callback1(errors, rawData) {
 
 	console.log(currentExample);
 
- // Adding words
-
-	var addWordDiv = d3.select("body")
+	var addDiv =  d3.select("body")
 		.append("div")
 		.style("float", "right");
+
+ // Adding single words
+
+	var addWordDiv = addDiv.append("div");
 
 	addWordDiv.append("input")
 		.attr("type", "text")
@@ -84,39 +86,101 @@ function callback1(errors, rawData) {
 		.on("click", function() {
 			var newWord = d3.select("#addWordInput").node().value;
 			console.log(newWord);
-			if (!(newWord in vecs)) {
+			if (!(newWord in vecs) || newWord.length == 0) {
 				addWordError.text("Word " + newWord + " not in dictionary.")
-					.style("display", "");
+					.style("visibility", "");
 			}
 			else {
 				d3.select("#addWordInput").node().value = "";
-				addWordError.style("display", "hidden");
-				currentExample.words.push([newWord]);
+				addWordError.style("visibility", "hidden");
 				currentExample.flat[newWord] = currentExample.groupsNumber++;
 				console.log(currentExample);
 				updateExample(currentExample);
 			}
 		});
 
-	var addWordError = addWordDiv.append("text")
-		.style("display", "hidden");
+
+
+// Adding a pair
+
+var addPairDiv = addDiv.append("div");
+
+	addPairDiv.append("input")
+		.attr("type", "text")
+		.attr("name", "addPairInput")
+		.attr("id", "addPairInput1")
+		.attr("class", "addPairInput");
+
+	addPairDiv.append("input")
+		.attr("type", "text")
+		.attr("name", "addPairInput")
+		.attr("id", "addPairInput2")
+		.attr("class", "addPairInput");
+
+	document.getElementById("addPairInput1")
+		.addEventListener("keyup", function(event) {
+	    event.preventDefault();
+	    if (event.keyCode == 13) {
+	        document.getElementById("addPairButton").click();
+	    }
+		});
+
+	document.getElementById("addPairInput2")
+		.addEventListener("keyup", function(event) {
+			event.preventDefault();
+			if (event.keyCode == 13) {
+					document.getElementById("addPairButton").click();
+			}
+		});
+
+	addPairDiv.append("input")
+		.attr("name", "addPairButton")
+		.attr("type", "button")
+		.attr("value", "Add pair")
+		.attr("id", "addPairButton")
+		.on("click", function() {
+			var newWord1 = d3.select("#addPairInput1").node().value;
+			var newWord2 = d3.select("#addPairInput2").node().value;
+			console.log(newWord1, newWord2);
+			if (!(newWord1 in vecs) || newWord1.length == 0) {
+				addWordError.text("Word " + newWord1 + " not in dictionary.")
+					.style("visibility", "");
+			}
+			else if (!(newWord2 in vecs) || newWord2.length == 0) {
+					addWordError.text("Word " + newWord2 + " not in dictionary.")
+						.style("visibility", "");
+			}
+			else {
+				d3.select("#addPairInput1").node().value = "";
+				d3.select("#addPairInput2").node().value = "";
+				addWordError.style("visibility", "hidden");
+				currentExample.flat[newWord2] = currentExample.groupsNumber;
+				currentExample.flat[newWord1] = currentExample.groupsNumber++;
+				console.log(currentExample);
+				updateExample(currentExample);
+			}
+		});
+
+	// word adding error display
+	var addWordError = addDiv.append("text")
+		.style("visibility", "hidden");
 
 	var plot = new Plot("body", currentPoints, currentExample);
 	updateExample(currentExample);
+
 	// plot updating
 	function updateExample() {
 		plot.updatePlot(getWithAxesJson(vecs, currentExample), currentExample);
 
 		// point deletion
 		d3.selectAll(".pointlabel")
-			.on("click", function (d) {
+			.on("dblclick", function (d) {
 				console.log(d);
 				for(var word in currentExample.flat) {
 	    		if(currentExample.flat.hasOwnProperty(word) && currentExample.flat[word] == d.group) {
 	        	delete currentExample.flat[word];
 					}
 				}
-				console.log
 				updateExample(currentExample);
 			});
 	}
